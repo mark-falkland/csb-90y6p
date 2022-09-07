3a5cc01fee252d2ce82e40507e2f85fc71be8498
Code Sandbox <hello@codesandbox.io>
2022-09-07 15:15:56 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.c26394bcd540f009b47b9714d3e3e9bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3fd4a31b822a428a3898fdf2b6fc63c",
+    "url": "https://mark-falkland.github.io/csb-90y6p/index.html"
+  },
+  {
+    "revision": "33b694716804b30eebf0",
+    "url": "https://mark-falkland.github.io/csb-90y6p/static/css/main.250d8404.chunk.css"
+  },
+  {
+    "revision": "fee3cd1f966fab3691a7",
+    "url": "https://mark-falkland.github.io/csb-90y6p/static/js/2.f225656d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "https://mark-falkland.github.io/csb-90y6p/static/js/2.f225656d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33b694716804b30eebf0",
+    "url": "https://mark-falkland.github.io/csb-90y6p/static/js/main.064b125b.chunk.js"
+  },
+  {
+    "revision": "96f9a8e7a88bf2996f51",
+    "url": "https://mark-falkland.github.io/csb-90y6p/static/js/runtime-main.beb305df.js"
+  }
+]);
